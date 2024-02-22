35ce7dab6f889d59b430f7e54e231a61a34f713e
BrattKondrattenko <artkondratenko3@gmail.com>
2024-02-22 16:20:33 +0300

    full realization

--- js/index.js
+++ js/index.js
@@ -10,22 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
         input: '[data-input]',
         button: '[data-button]',
         modal: '[data-modal]',
+        modalEdit: '[data-edit]',
         inputModal: '[data-input-modal]',
         modalContent: '.modal__content',
         modalCloseButton: '.modal__close',
-        buttonShow: '.modal__button-show'
+        buttonShow: '.modal__button-show',
+        editButton: '.edit-contact',
+        inputsEdit: '[data-edit]',
+        saveButton: '[data-save]'
     };
 
     const rootElement = document.querySelector(selectors.root);
     const modalWindow = rootElement.querySelector(selectors.modal);
+    const modalEditWindows = rootElement.querySelector(selectors.modalEdit)
     const inputModal = rootElement.querySelector(selectors.inputModal);
     const modalContent = rootElement.querySelector(selectors.modalContent);
     const modalCloseButton = rootElement.querySelector(selectors.modalCloseButton);
     const buttonShow = rootElement.querySelector(selectors.buttonShow);
+    const editButton = rootElement.querySelector(selectors.editButton)
+    const saveButton = rootElement.querySelector(selectors.saveButton)
 
-    const nameInput = rootElement.querySelector(`${selectors.input}[data-type="name"]`)
-    const positionInput = rootElement.querySelector(`${selectors.input}[data-type="name"]`)
-    const phoneInput = rootElement.querySelector(`${selectors.input}[data-type="name"]`)
+    const inputsEdit = document.querySelectorAll(selectors.inputsEdit)
 
     const inputElements = rootElement.querySelectorAll(selectors.input);
     const addButtonElement = rootElement.querySelector(`${selectors.button}[data-action="add"]`);
@@ -34,36 +39,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const columnItems = document.querySelectorAll('.item')
     const getFirstLetter = word => word[0].toUpperCase();
-    const clearInput = () => inputElements.forEach(input => input.value = '');
+    const clearInput = () => {
+        inputElements.forEach(input => input.value = '');
+        inputsEdit.forEach(input => input.value = '');
+    }
+    const isLengthValid = (value, minLength) => value.trim().length >= minLength;
 
     const updateContactsState = () => contacts = { ...initialState };
     const resetUI = () => document.querySelectorAll('.added-contact').forEach(div => div.remove());
     const resetCount = () => document.querySelectorAll('.count-element').forEach(count => count.remove())
 
-    nameInput.addEventListener('input', (e) => {
-        console.log(e.target.value)
-        console.log(isLengthValid(e.target.value, 4))
-        if (!isLengthValid(e.target.value, 4)) {
-            e.target.classList.add('error')
-        } else {
-            e.target.classList.remove('error')
-        }
-    })
+    document.querySelectorAll('[data-input]').forEach(input => {
+        input.addEventListener('input', () => {
+            let isValid = true;
+            const value = input.value.trim();
+            const type = input.getAttribute('data-type');
 
-    positionInput.addEventListener('input', (e) => {
-    })
+            if (value.length < 4) isValid = false;
 
-    phoneInput.addEventListener('input', (e) => {
-        if (!isPhoneValid(e.target.value)) {
-        }
+            if (type === 'phone' && !isPhoneValid(value)) isValid = false
+
+            input.classList.toggle('error', !isValid);
+        });
     })
 
     const isEmpty = (value) => value.trim() === '';
 
-    const isLengthValid = (value, minLength) => value.trim().length >= minLength;
-
     const isPhoneValid = (phone) => /^\+\d{2,15}$/.test(phone);
-
 
     const renderCount = () => {
         Object.keys(contacts).forEach(firstLetter => {
@@ -88,6 +90,10 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     const renderContacts = (contact, firstLetter, container = null) => {
+        if (!contact.id) {
+            return;
+        }
+
         const contactContainer = container || document.querySelector(`[data-first-letter="${firstLetter}"]`) || createContactContainer(firstLetter);
         const contactElement = document.createElement("div");
         contactElement.classList.add('added-contact', 'hidden');
@@ -95,10 +101,13 @@ document.addEventListener('DOMContentLoaded', function() {
         if (contactContainer)
         contactElement.setAttribute('data-contact-id', contact.id);
         contactElement.innerHTML = `
-    id:${contact.id}<br> name:${contact.name}<br> phone:${contact.phone}<br>
-    <span class="delete-contact" data-contact-id="${contact.id}">X</span>`;
+        name:${contact.name}<br> position:${contact.position}<br> phone:${contact.phone}<br>
+        <span class="delete-contact" data-contact-id="${contact.id}">X</span>
+        <span class="edit-contact hidden" data-contact-id="${contact.id}">EDIT</span>`;
         contactContainer.appendChild(contactElement);
+
         addDeleteEventListener(contactElement.querySelector('.delete-contact'));
+        addEditEventListener(editButton, contact)
     };
 
 
@@ -117,12 +126,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const phone = inputElements[2].value;
 
         if (isEmpty(name) || isEmpty(position) || isEmpty(phone)) {
-            alert('Empty')
+            alert('Заполните все поля')
             return
         }
+
         const contact = { id: Date.now(), name, position, phone };
         addContact(contact);
-
     };
 
     const removeContact = (firstLetter, contactId) => {
@@ -136,7 +145,6 @@ document.addEventListener('DOMContentLoaded', function() {
         renderCount()
     };
 
-    // Создание контейнера для контакта, если он еще не существует
     const createContactContainer = (firstLetter) => {
         const container = document.createElement("div");
         container.setAttribute('data-first-letter', firstLetter);
@@ -144,8 +152,24 @@ document.addEventListener('DOMContentLoaded', function() {
         return container;
     };
 
-// Добавление слушателя событий на кнопку удаления
-    const addDeleteEventListener = deleteButton => {
+    const updateContact = (contactId, updatedContact) => {
+        Object.keys(contacts).forEach(letter => {
+            const index = contacts[letter].findIndex(contact => contact.id === parseInt(contactId));
+            console.log(index)
+            if (index !== -1) {
+                contacts[letter][index] = { ...contacts[letter][index], ...updatedContact };
+                console.log(contacts[letter][index])
+            }
+        });
+
+        resetUI();
+        Object.keys(contacts).forEach(letter => {
+            contacts[letter].forEach(contact => renderContacts(contact, letter));
+        });
+        renderCount();
+    }
+
+    const addDeleteEventListener = (deleteButton) => {
         deleteButton.addEventListener('click', () => {
             const contactId = parseInt(deleteButton.dataset.contactId); // Используйте .dataset для доступа к атрибутам данных
             let firstLetter = null;
@@ -164,7 +188,15 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     };
-
+    const addEditEventListener = (editButton, contact) => {
+        editButton.addEventListener('click', () => {
+            modalEditWindows.classList.toggle('active')
+            modalEditWindows.setAttribute('data-current-contact-id', contact.id);
+            inputsEdit[1].value = contact.name
+            inputsEdit[2].value = contact.position
+            inputsEdit[3].value = contact.phone
+        })
+    }
     const setupEventListeners = () => {
         addButtonElement.addEventListener('click', handleAddContact);
         clearButtonElement.addEventListener('click', handleClearContacts);
@@ -181,6 +213,22 @@ document.addEventListener('DOMContentLoaded', function() {
                      })
                 }
             })
+        })
+        saveButton.addEventListener('click', () => {
+            const contactId = modalEditWindows.getAttribute('data-current-contact-id');
+            const name = inputsEdit[1].value;
+            const position = inputsEdit[2].value;
+            const phone = inputsEdit[3].value;
+
+            if (contactId) {
+                updateContact(contactId, { name, position, phone });
+            } else {
+                const newContact = { id: Date.now(), name, position, phone };
+                addContact(newContact);
+            }
+
+            modalEditWindows.classList.remove('active');
+            clearInput();
         })
     };
 
@@ -200,10 +248,13 @@ document.addEventListener('DOMContentLoaded', function() {
                     modalContent.querySelectorAll('div.added-contact').forEach((item) => {
                         item.classList.remove('hidden')
                     })
+
+                    modalContent.querySelectorAll('.edit-contact').forEach((item) => {
+                        item.classList.remove('hidden')
+                    })
                 }
             });
         });
-
     };
 
     setupEventListeners();
